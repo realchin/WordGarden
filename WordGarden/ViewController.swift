609c425b373756bf3fc2c64f8ecc530e6ed427b3
Timothy Chin <chinti@bc.edu>
2022-02-10 00:23:51 -0500

    Section 3.5

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -36,8 +36,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
-        let text = guessedLetterTextField.text!
-        guessLetterButton.isEnabled = !(text.isEmpty)
+        sender.text = String(sender.text?.last ?? " ").trimmingCharacters(in: .whitespaces)
+        guessLetterButton.isEnabled = !(sender.text!.isEmpty)
     }
     
     @IBAction func doneKeyPressed(_ sender: UITextField) {
